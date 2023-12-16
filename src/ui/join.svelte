36a193c6178f323cebf3b0fce4813fdc64c21211
andreecy <andreechristianyulianto@gmail.com>
2023-12-16 22:40:58 +0700

    add tailwind

--- src/ui/join.svelte
+++ src/ui/join.svelte
@@ -1,20 +1,14 @@
 <script lang="ts">
   import { supabaseClient } from "../lib/supabase";
-
 </script>
 
-<div id="ui-join">hello supagame
+<div class="absolute w-full flex flex-col items-center justify-center">
+  <h1 class="text-3xl font-bold">Supagame</h1>
 
+  <div class="my-4">
+    <input type="text" class="bg-white rounded border-2 border-blue-500" />
+  </div>
 </div>
 
-<!-- Basic styling -->
 <style>
-#ui-join {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    color: white;
-    background-color: gray;
-    
-}
 </style>
